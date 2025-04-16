928a4c2f94b7486ba11ebe160acd9c589078eff6
Vedant <svedant999@gmail.com>
2025-04-16 11:38:26 +0530

    Automation Basic Flow

---
+++ src/test/java/Test/Checkvalidation.java
@@ -0,0 +1,21 @@
+package Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+
+public class Checkvalidation {
+    public static void main(String[] args) {
+        String title="Facebook – log in or sign up";
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.facebook.com/");
+        Assert.assertEquals(driver.getTitle(), title);
+    }
+
+
+}
